95e56ca583ed1641408a2e2210adeb22208c01fc
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-09-29 22:57:27 +0530

    Create ShowMail.java

---
+++ MailApplication/ShowMail.java
@@ -0,0 +1,34 @@
+package mailmanagementsystem;
+public class ShowMail
+{
+	public void showAllMails(String mail)throws java.io.IOException
+	{
+            try
+            {
+            Class.forName("com.mysql.jdbc.Driver");
+            String sql="SELECT * FROM sender WHERE receiveremailid='"+mail+"' OR senderemailid='"+mail+"'";
+            java.sql.Connection conn = java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/mailmanagementsystem?zeroDateTimeBehavior=convertToNull","root","");
+            java.sql.PreparedStatement ps = conn.prepareStatement(sql);
+            java.sql.ResultSet r=ps.executeQuery();
+            while(true)
+            {
+                if(r.next())
+                {
+                    System.out.println("-------------------------------------");
+                    System.out.println("SENDER : "+r.getString("senderemailid"));
+                    System.out.println("RECEIVER : "+r.getString("receiveremailid"));
+                    System.out.println("SUBJECT : "+r.getString("subject"));
+                    System.out.println("CONTENT : "+r.getString("content"));
+                    System.out.println("======================================");
+                }
+                else
+                {
+                    System.out.println("                             >>>   NOTHING MORE   <<<");
+                    break;
+
+                }
+            }
+            
+        }catch(Exception e){}
+	}
+}
