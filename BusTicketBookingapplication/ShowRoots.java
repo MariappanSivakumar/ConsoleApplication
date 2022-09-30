a9b9dfd1f08cbf50ff38cd9ad690685675324e43
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-09-30 19:35:04 +0530

    Create ShowRoots.java

---
+++ BusTicketBookingapplication/ShowRoots.java
@@ -0,0 +1,14 @@
+
+package busticketbookingapplication;
+
+
+public class ShowRoots {
+    public void showroots( int n )
+    {
+        new Destination().display();
+        if(n==1)
+        {
+            System.out.println("");
+        }
+    }
+}
